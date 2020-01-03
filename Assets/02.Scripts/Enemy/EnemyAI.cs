1d72d1ff6c899b498a63e85a14986094273d0887
Sa Myoung Jin <ekdskrnl105@gmail.com>
2020-01-03 23:14:09 +0900

    Animation Blending - Blend_Tree, Enemy 회전의 구면선형보간 적용

--- Assets/02.Scripts/Enemy/EnemyAI.cs
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -22,6 +22,10 @@ public class EnemyAI : MonoBehaviour
     //적 캐릭터의 위치를 저장할 변수
     private Transform enemyTr;
 
+    //Animator 컴포넌트를 저장할 변수
+    private Animator animator;
+
+
     //공격 사정거리
     public float attackDist = 5.0f;
 
@@ -34,6 +38,15 @@ public class EnemyAI : MonoBehaviour
     //코루틴에서 사용할 지연시간 변수
     private WaitForSeconds ws;
 
+    //이동을 제어하는 MoveAgent 의 클래스를 저장할 변수
+    private MoveAgent moveAgent;
+
+    //애니메이터 컨트롤러에 정의한 파라미터의 해시값을 미리 추출
+    private readonly int hashMove = Animator.StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
+
+
+    
 
     void Awake()
     {
@@ -48,7 +61,14 @@ public class EnemyAI : MonoBehaviour
             //적 캐릭터의 Transform 컴포넌트 추출
             enemyTr = GetComponent<Transform>();
 
-            //코루틴의 지연 시간 생성
+            //적 캐릭터의 Transform 컴포넌트를 추출
+            animator = GetComponent<Animator>();
+
+
+            //이동을 제어하는 MoveAgent 클래스를 추출
+            moveAgent = GetComponent<MoveAgent>();
+
+            //코루틴의 지연 시간 생성1
             ws = new WaitForSeconds(0.3f);
 
         }
@@ -87,7 +107,47 @@ public class EnemyAI : MonoBehaviour
     {
         //CheckState 코루틴 함수 실행
         StartCoroutine(CheckState());
+
+        //Action 코루틴 함수 실행
+        StartCoroutine(Action());
     }
+
+    //상태에 따라 적 캐릭터의 행동을 처리하는 코루틴 함수
+    IEnumerator Action()
+    {
+        //적 캐릭터가 사망할 때 까지 무한 루프
+        while(!isDie)
+        {
+            yield return ws;
+            //상태에 따른 분기 처리
+            switch(state)
+            {
+                case State.PATROL:
+                    //순찰 모드를 활성화
+                    moveAgent.patrolling = true;
+                    animator.SetBool(hashMove, true);
+
+                    break;
+                case State.TRACE:
+                    //주인공의 위치를 넘겨 추적 모드로 변경
+                    moveAgent.traceTarget = playerTr.position;
+
+                    animator.SetBool(hashMove, true);
+                    break;
+                case State.ATTACK:
+                    //순찰 및 추적을 정지
+                    moveAgent.Stop();
+                    animator.SetBool(hashMove, false);
+
+                    break;
+                case State.DIE:
+                    //순찰 및 추적을 정지
+                    moveAgent.Stop();
+                    break;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -97,6 +157,7 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Speed 파라미터에 이동 속도를 전달
+        animator.SetFloat(hashSpeed, moveAgent.speed);
     }
 }
